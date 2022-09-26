6658952ecaf3357064b4a433ab78c343d7e85582
snow-fox <taijunjet@hotmail.com>
2022-09-26 14:42:50 +0100

    update floaterm to have silent mode

--- lua/plugin/floaterm-plugin.lua
+++ lua/plugin/floaterm-plugin.lua
@@ -4,14 +4,22 @@ vim.g.floaterm_wintype = 'float'
 vim.g.floaterm_position = 'right'
 vim.g.floaterm_title = 'Terminal $1|$2'
 vim.g.floaterm_borderchars = '─│─│╭╮╯╰'
-vim.g.floaterm_autoinsert = true
+vim.g.floaterm_autoinsert = false
 
 -- colours
 vim.api.nvim_set_hl(0, 'Floaterm', {})
 vim.api.nvim_set_hl(0, 'FloatermBorder', { bg = "#303035" })
 
--- plugins
+-- floaterm function with insert mode
+function FloatermInsertEnter()
+    vim.g.floaterm_autoinsert = true
+    vim.cmd('FloatermToggle')
+    vim.g.floaterm_autoinsert = false
+end
+
+-- mappings
+vim.api.nvim_set_keymap('n', '<A-j>', ':lua FloatermInsertEnter()<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<A-k>', ':FloatermToggle<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<A-j>', ':FloatermToggle<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true})
+
