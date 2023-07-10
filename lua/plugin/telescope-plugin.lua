a5fad50a035b97dc3239f34a72c33e5fed2f53d1
jjshoots <taijunjet@hotmail.com>
2023-07-10 13:30:44 +0100

    find even in gitignore if allfind

--- lua/plugin/telescope-plugin.lua
+++ lua/plugin/telescope-plugin.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>p', ':Telescope find_files hidden=true<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>p', ':Telescope find_files hidden=true no_ignore=true<CR>', {})
 
 require('telescope').setup{
   defaults = {
