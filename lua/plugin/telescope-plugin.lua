b9df1c1fba04a3b58d4a8e8f9016419007a31c95
jjshoots <taijunjet@hotmail.com>
2023-08-14 16:45:54 +0100

    livegrep yo

--- lua/plugin/telescope-plugin.lua
+++ lua/plugin/telescope-plugin.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_set_keymap('n', '<M-p>', ':Telescope find_files<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>p', ':Telescope find_files hidden=true no_ignore=true<CR>', {})
+vim.api.nvim_set_keymap('n', '<M-q>', ':Telescope live_grep<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>q', ':Telescope live_grep hidden=true no_ignore=true<CR>', {})
 
 require('telescope').setup{
   defaults = {
