c560408ad4ae075f8c5779c0e4ebe9864345e89c
Jet <taijunjet@hotmail.com>
2024-03-01 15:33:03 +0000

    fix indentations

--- lua/plugin/treesitter-plugin.lua
+++ lua/plugin/treesitter-plugin.lua
@@ -19,6 +19,9 @@ require("nvim-treesitter.configs").setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
+  indent = {
+    enable = true,
+  }
 })
 
 -- let rainbow handle brackets
