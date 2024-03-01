9eda27ee6e183a4b1f45736d2a92283b825a780e
Jet <taijunjet@hotmail.com>
2024-03-01 12:34:05 +0000

    NO INLAY HINTS FINALLY

--- lua/plugin/lsp_setup.lua
+++ lua/plugin/lsp_setup.lua
@@ -37,6 +37,11 @@ require("mason-lspconfig").setup({
 	},
 })
 
+-- disable inlay hints
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
+})
+
 -- AUTOCOMPLETION
 local cmp = require("cmp")
 cmp.setup({
