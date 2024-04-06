9708e78cac1b396a2cca57fdba9319f39d84b769
Jet <taijunjet@hotmail.com>
2024-04-06 15:30:21 +0100

    try autosnippets and style

--- lua/plugins/plugin-luasnip.lua
+++ lua/plugins/plugin-luasnip.lua
@@ -1,22 +1,33 @@
 local function config_function()
-  require'luasnip'.config.set_config {
-      history = true,
-      updateevents = "TextChanged,TextChangedI",
-  }
-  require("luasnip.loaders.from_vscode").lazy_load()
+	require("luasnip").config.set_config({
+		history = true,
+		updateevents = "TextChanged,TextChangedI",
+		enable_auto_snippets = true,
+	})
+	require("luasnip.loaders.from_vscode").lazy_load()
 
-  -- Expand or jump to the next snippet placeholder
-  vim.api.nvim_set_keymap('i', '<C-m>', 'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<Tab>"', {expr = true, silent = true})
-  -- Jump to the previous snippet placeholder
-  vim.api.nvim_set_keymap('i', '<C-n>', 'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<S-Tab>"', {expr = true, silent = true})
+	-- Expand or jump to the next snippet placeholder
+	vim.api.nvim_set_keymap(
+		"i",
+		"<C-m>",
+		'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<Tab>"',
+		{ expr = true, silent = true }
+	)
+	-- Jump to the previous snippet placeholder
+	vim.api.nvim_set_keymap(
+		"i",
+		"<C-n>",
+		'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<S-Tab>"',
+		{ expr = true, silent = true }
+	)
 end
 
 return {
-  "L3MON4D3/LuaSnip",
+	"L3MON4D3/LuaSnip",
 	dependencies = {
-    "rafamadriz/friendly-snippets",
+		"rafamadriz/friendly-snippets",
 	},
-  build = "make install_jsregexp",
+	build = "make install_jsregexp",
 	config = config_function,
 	event = "LspAttach",
 }
