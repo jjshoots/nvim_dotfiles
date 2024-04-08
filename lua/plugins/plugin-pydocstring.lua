9928659def12e7a41adb6f06f6c220945e448e05
Jet <taijunjet@hotmail.com>
2024-04-08 17:58:30 +0100

    fkin pydocstring

--- lua/plugins/plugin-pydocstring.lua
+++ lua/plugins/plugin-pydocstring.lua
@@ -1,9 +1,10 @@
 local function config_function()
-  vim.g.pydocstring_enable_mapping = 0
+	vim.g.pydocstring_enable_mapping = 0
+	vim.g.pydocstring_formatter = "google"
 end
 
 return {
 	"heavenshell/vim-pydocstring",
 	config = config_function,
-	ft = "python",
+	build = "make install",
 }
