f0fbec576fd1a17af99a4010ce016dda2ae5720b
Jet <taijunjet@hotmail.com>
2024-06-03 23:31:42 +0900

    install nvim if not installed

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 sudo apt update
 sudo apt install git ripgrep fd-find --yes
 
+# check if nvim exists
+if ! command -v nvim &> /dev/null
+then
+    echo "nvim not found, installing from snap"
+    sudo snap install nvim --classic
+fi
+
 # Install neovim
 sudo snap install neovim --classic
 
