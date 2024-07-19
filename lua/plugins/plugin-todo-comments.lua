ccbc3652c7e84b262961ed2902974d0658a78c2a
Jet <junjet.tai@sony.com>
2024-07-19 13:31:37 +0900

    activate on command

--- lua/plugins/plugin-todo-comments.lua
+++ lua/plugins/plugin-todo-comments.lua
@@ -1,11 +1,8 @@
-local function config_function()
-end
-
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
-  keys = {
-    { "<leader>d", ":TodoTelescope keywords=TODO,FIX,BUG,WARN,WARNING,PERF <CR>", mode = "n", noremap = true, silent = true },
-  },
+  cmd = {
+    "TodoTelescope",
+  }
 }
