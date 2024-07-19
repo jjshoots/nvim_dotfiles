d22c5253ad517c7a73bdd91f743b4589435ae24e
Jet <junjet.tai@sony.com>
2024-07-19 13:24:32 +0900

    use gitsigns

---
+++ lua/plugins/plugin-gitsigns.lua
@@ -0,0 +1,9 @@
+local function config_function()
+  require("gitsigns").setup()
+end
+
+return {
+	"lewis6991/gitsigns.nvim",
+	config = config_function,
+	event = "VeryLazy",
+}
---
+++ vim/set.vim
@@ -0,0 +1,1 @@
+let g:pydocstring_doq_path = '/home/dev/venv/bin/doq'
