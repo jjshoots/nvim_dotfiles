65cf113ac7e8f2a7ae35ae9ba7d8c6fd49bf7d58
Jet <junjet.tai@sony.com>
2024-07-20 14:42:18 +0900

    add filepath copy

---
+++ lua/manual_funcs.lua
@@ -0,0 +1,7 @@
+-- copy filepath of current file to clipboard
+local function copy_filepath_to_clipboard()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  vim.fn.setreg('+', filepath)
+  print("File path copied: " .. filepath)
+end
+vim.api.nvim_create_user_command('CopyFilePath', copy_filepath_to_clipboard, {})
