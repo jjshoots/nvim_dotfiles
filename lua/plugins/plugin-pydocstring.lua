cd107852ff6811209cbcaa006ad5bad396317baa
Jet <taijunjet@hotmail.com>
2024-07-30 00:34:44 +0900

    stylua

--- lua/plugins/plugin-pydocstring.lua
+++ lua/plugins/plugin-pydocstring.lua
@@ -2,10 +2,10 @@ local function config_function()
 	vim.g.pydocstring_enable_mapping = 0
 	vim.g.pydocstring_formatter = "google"
 
-  local handle = io.popen("which doq")
-  local doq_path = handle:read("*a"):gsub("%s+", "") -- Remove any trailing whitespace
-  handle:close()
-  vim.g.pydocstring_doq_path = doq_path
+	local handle = io.popen("which doq")
+	local doq_path = handle:read("*a"):gsub("%s+", "") -- Remove any trailing whitespace
+	handle:close()
+	vim.g.pydocstring_doq_path = doq_path
 end
 
 return {
