e2aeb14a4122c37720f56cd7e1c88bd626cd910e
Jet <38184875+jjshoots@users.noreply.github.com>
2025-01-23 13:47:14 -0800

    Create install_macos.sh

---
+++ install_macos.sh
@@ -0,0 +1,11 @@
+# Update Homebrew
+brew update
+
+# Install Neovim
+brew install neovim
+
+# Install dependencies
+brew install git ripgrep fd npm curl
+
+# Clone the repo
+git clone https://github.com/jjshoots/nvim_dotfiles.git ~/.config/nvim
